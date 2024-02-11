dd6b96d09a21fad497cba0ea36daad21f83f2b49
NikitaTitov <titovn811@gmail.com>
2024-02-11 11:40:13 +0700

    Added sentry and additional requirements

--- faker_job/settings.py
+++ faker_job/settings.py
@@ -10,8 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 import os
 from pathlib import Path
+import sentry_sdk
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,6 +92,9 @@ DATABASES = {
     }
 }
 
+sentry_sdk.init(
+    dsn = os.getenv('SENTRY_KEY'),
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- faker_job/urls.py
+++ faker_job/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+def trigger_error(request):
+    print('asd' < 1)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
